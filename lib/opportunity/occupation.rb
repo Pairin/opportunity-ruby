9196c2721ae1a537e66cba9bdb412beee88233da
Alejandro Vasquez <avazquez@pairin.com>
2017-07-20 12:20:53 -0600

    PAT-35 add occupation name search

--- lib/opportunity/occupation.rb
+++ lib/opportunity/occupation.rb
@@ -2,6 +2,14 @@ module Opportunity
   class Occupation < APIResource
     extend APIActions::List
 
+    class << self
+
+      def custom_accepted_params
+        ['title']
+      end
+
+    end
+
     def programs(params={})
       Program.list({occupation_id: self.id}.merge!(params))
     end
