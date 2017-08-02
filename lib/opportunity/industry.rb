b4c5b8dc2cae9a8259028c3a7ca8863090901c8a
Alejandro Vasquez <avazquez@pairin.com>
2017-08-02 10:01:13 -0600

    industry: add title as accepted param

--- lib/opportunity/industry.rb
+++ lib/opportunity/industry.rb
@@ -1,5 +1,14 @@
 module Opportunity
   class Industry < APIResource
     extend APIActions::List
+
+    class << self
+
+      def custom_accepted_params
+        ['title']
+      end
+
+    end
+
   end
 end
