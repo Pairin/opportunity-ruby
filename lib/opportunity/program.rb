da6eabeb2a8d6a76bcae6e0524704b82a7c8beb2
Alejandro Vasquez <avazquez@pairin.com>
2017-09-18 08:36:09 -0600

    add Program Level child

--- lib/opportunity/program.rb
+++ lib/opportunity/program.rb
@@ -1,3 +1,6 @@
+# Child Objects
+require 'opportunity/programs/level'
+
 module Opportunity
   class Program < APIResource
     extend APIActions::List
@@ -7,6 +10,12 @@ module Opportunity
 
       def custom_accepted_params
         ['occupation_id', 'level_id', 'institution_id']
+      end
+
+      def child_mappings
+        {
+          levels: Programs::Level
+        }
       end
 
     end
---
+++ lib/opportunity/programs/level.rb
@@ -0,0 +1,6 @@
+module Opportunity
+  module Programs
+    class Level < OpportunityObject
+    end
+  end
+end
