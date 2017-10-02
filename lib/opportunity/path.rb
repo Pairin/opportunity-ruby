d55cafa1ecec2c10100c7056b8a298e737ec04ec
Alejandro Vasquez <avazquez@pairin.com>
2017-10-02 12:57:16 -0600

    path: make child inputs optional, add lat,long

--- lib/opportunity/path.rb
+++ lib/opportunity/path.rb
@@ -11,8 +11,10 @@ module Opportunity
           'start_point' => '',
           'end_point' => '',
           'inputs' => {
-            'skill_set_id' => '',
-            'award_level' => ''
+            '~skill_set_id' => '',
+            '~award_level' => '',
+            '~lat' => 0.0,
+            '~long' => 0.0
           }
         }.freeze
       end
